da80f00e65abd4db63bcdb564b6b18669b8a9eb8
Alia Nguyen <trangkiny@gmail.com>
2022-10-04 11:50:10 +0300

    create project

---
+++ README.md
@@ -0,0 +1,8 @@
+# TypeScript Assignments
+
+Check the questions in index.ts
+
+### To run the project
+1. `npm install`: install all the dependencies
+2. `npm start` or `yarn start`: compile the ts file, form js file and run the code
+3. You can use `console.log()` to see the result in your terminal
---
+++ src/index.ts
@@ -0,0 +1,56 @@
+// TypeScript is required
+
+const testArr = [1, 4, 7, 19, 35, 21, 69, 34, 5, 15]
+
+
+/** SortArray class should receive input of type array of number
+ * getMaxIndex(),  getMinIndex(), getMedianIndex() are private methods
+ * 
+ */
+class SortArray {
+    constructor(input) {
+
+    }
+
+    /** this returns the index of biggest item in the array */
+    getMaxIndex() {
+
+    }
+
+    /** this return the  biggest item in the array. Reuse getMaxIndex*/
+    getMaxElement() {
+
+    }
+
+
+    /** this return the index of smallest item in the array */
+    getMinIndex() {
+
+    }
+
+    /** this return the smallest item in the array. Reuse the getMinIndex*/
+    getMinElement() {
+
+    }
+
+    /** this return the index of median item in the array 
+     * to find the median item, you need to sort the array, then use the 
+     * algorithm: data[Math.floor(data.length / 2)] */
+    getMedianIndex() {
+    }
+
+
+    /** this return the median item in the array. Reuse getMedianIndex */
+    getMedianElement() {
+
+    }
+
+    /** this return the average value of the array*/
+    getAverageValue() {
+
+    }
+}
+
+const caculationResult = new SortArray(testArr)
+/* run the methods to get the min, max, median, average values with the
+testArr. Share your results in Slack and compare with your friends */
